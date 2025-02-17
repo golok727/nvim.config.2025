18b65f28c715c35ca4e2963013b3425cc62344fc
Aadi <goooolok727@gmail.com>
2025-02-17 13:10:47 +0530

    theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,23 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", lazy = false, opts = {} },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    opts = {
+      color_overrides = {
+        mocha = {
+          base = "#0b0c10",
+          mantle = "#0b0c10",
+          crust = "#0b0c10",
+        },
+      },
+    },
+  },
 }
